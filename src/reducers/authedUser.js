c6376eb59c610463ce94d4f5b7d18848e521a7f5
Ryan Cherry <rcherryman8@gmail.com>
2023-10-05 09:38:03 -0400

    Udacity Project 2 Dependency fix 2

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,12 +1,12 @@
-import { SET_CURRENT_USER, REMOVE_CURRENT_USER } from '../actions/authedUser'
+import { SET_CURRENT_USER, REMOVE_CURRENT_USER } from '../actions/authedUser';
 
-export default function (state = null, action) {
+export default function authedUser(state = null, action) {
     switch (action.type) {
-        case SET_CURRENT_USER :
-            return action.id
-        case REMOVE_CURRENT_USER :
-            return action.id
-        default :
-            return state
+        case SET_CURRENT_USER:
+            return action.id;
+        case REMOVE_CURRENT_USER:
+            return action.id;
+        default:
+            return state;
     }
 }
